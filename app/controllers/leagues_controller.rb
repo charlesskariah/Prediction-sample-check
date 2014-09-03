f3a5cfc0328a30b92eda7dea49bf973e57115d77
Charles Skariah <charles@charles-Pc.(none)>
2014-09-03 19:51:34 +0530

    Modifications made for some changes

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -5,9 +5,7 @@ class LeaguesController < ApplicationController
     user_id = current_user.id
     #@unjoined_leagues =  League.find_by_sql ("SELECT * FROM leagues WHERE id NOT IN (SELECT league_id from selected_leagues where user_id = user_id)")
     @unjoined_leagues = @leagues - @joined_leagues
-    p "============"
-    p @unjoined_leagues
-    
+
   end
 
   def join_league
