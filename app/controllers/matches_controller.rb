0cb3e1d576fc6824d340b08b0e27bc5c7f57ea05
Awin Abi <awinabi@gmail.com>
2014-09-05 19:06:45 +0530

    Fix League link to point to upcoming matches for the league

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,8 +1,12 @@
 class MatchesController < ApplicationController
   def view_matches
-  	round_id = params[:round_id]
-    @matches = Match.where(:round_id => round_id).order(:match_date)
-    @league = Round.find(round_id).league
+    round_id = params[:round_id]
+    if round_id
+      @matches = Match.where(:round_id => round_id).order(:match_date)
+      @league = Round.find(round_id).league
+    elsif params[:league_id].present?
+      @league = League.find(params[:league_id])
+    end
     @selected_league = SelectedLeague.where(league_id: @league.id, user_id: current_user.id).to_a.first
     @prediction = Prediction.new
   end
--- app/models/league.rb
+++ app/models/league.rb
@@ -6,6 +6,6 @@ class League < ActiveRecord::Base
   has_and_belongs_to_many :clubs
 
   def upcoming_round
-  	Match.where("match_date >= ?", Date.today).order(:match_date).first.try(:round)
+    Match.where("match_date >= ? AND round_id IN(?)", Date.today, rounds.map(&:id)).order(:match_date).first.try(:round)
   end
 end
