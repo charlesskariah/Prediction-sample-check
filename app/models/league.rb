5a6f1d67531e8c720235995a7bd0dbfcbe7544f4
Charles Skariah <charles@charles-Pc.(none)>
2014-09-05 17:10:19 +0530

    change in matches controller and view

--- app/models/league.rb
+++ app/models/league.rb
@@ -1,9 +1,11 @@
+require 'date'
 class League < ActiveRecord::Base
   has_many :rounds
   has_many :selected_leagues
   has_many :users, :through => :selected_leagues
   has_and_belongs_to_many :clubs
 
-  def current_round
+  def upcoming_round
+  	Match.where("match_date >= ?", Date.today).order(:match_date).first.try(:round)
   end
 end
--- app/models/round.rb
+++ app/models/round.rb
@@ -3,8 +3,10 @@ class Round < ActiveRecord::Base
   belongs_to :league
 
   def previous_round
+  	league.upcoming_round.try(:id) - 1 if league.upcoming_round
   end
 
   def next_round
+  	league.upcoming_round.try(:id) + 1 if league.upcoming_round
   end
 end
