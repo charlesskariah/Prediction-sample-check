96572a019f90abfd23b1508024796f6882e54e1c
Charles Skariah <charles@charles-Pc.(none)>
2014-09-08 18:34:29 +0530

    Methods for restricting user from creating and updating score

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
 
-  validates :firstname, :username, :country, :dob, presence:true
+  validates :firstname, :username, :country, :dob, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
