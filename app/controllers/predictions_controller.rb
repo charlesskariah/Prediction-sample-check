6c13ed86db43feee026f7899248c43455fcb8d8f
Charles Skariah <charles@charles-Pc.(none)>
2014-09-09 14:50:33 +0530

    Validations at model level for predictions

--- app/controllers/predictions_controller.rb
+++ app/controllers/predictions_controller.rb
@@ -7,36 +7,27 @@ class PredictionsController < ApplicationController
   end
 
   def create
-    current_round_id = Match.where("match_date >= ? ", Date.today).order(:match_date).first.round_id
-    if params[:round_id].to_i < current_round_id
-       round_id = params[:round_id]
-       @prediction = Prediction.new(prediction_params)
-       @league = @prediction.match.round.league
-       redirect_to :controller => 'matches', :action => 'view_matches',notice: "You are not able to predict completed Matches!!", round_id: round_id
-    else
       @prediction = Prediction.new(prediction_params)
-      @prediction.save
-      round_id = params[:round_id]
-      @league = @prediction.match.round.league
-      redirect_to :controller => 'matches', :action => 'view_matches',notice: "Predicted Successfully!!", round_id: round_id
-    end
+      if @prediction.save
+        round_id = params[:round_id]
+        @league = @prediction.match.round.league
+        redirect_to :controller => 'matches', :action => 'view_matches',notice: "Prediction Created!!", round_id: round_id
+      else 
+        round_id = params[:round_id]
+        @league = @prediction.match.round.league
+        redirect_to :controller => 'matches', :action => 'view_matches',notice: "Prediction is not possible", round_id: round_id
+      end
   end
 
   def update
-    current_round_id = Match.where("match_date >= ? ", Date.today).order(:match_date).first.round_id
-    if params[:round_id].to_i < current_round_id
-      round_id = params[:round_id]
-      @prediction = Prediction.find(params[:id])
-      @league = @prediction.match.round.league
-      redirect_to :controller => 'matches', :action => 'view_matches',notice: "You are not able to update completed matches!!", round_id: round_id
-    else
       round_id = params[:round_id]
       @prediction = Prediction.find(params[:id])
       @league = @prediction.match.round.league
       if @prediction.update(prediction_params)
         redirect_to :controller => 'matches', :action => 'view_matches',notice: "Prediction Updated!!", round_id: round_id
+      else 
+        redirect_to :controller => 'matches', :action => 'view_matches',notice: "No more predictions for this match!!", round_id: round_id
       end
-    end
   end
 
   def delete
--- app/models/prediction.rb
+++ app/models/prediction.rb
@@ -1,5 +1,14 @@
 class Prediction < ActiveRecord::Base
-  validates :club1_predicted_score, :club2_predicted_score, presence: true
+  #validates :club1_predicted_score, :club_2_predicted_score, presence: true,
+  validate :check_for_matchdate
+  validates :club1_predicted_score, :club2_predicted_score, :presence => {:message => "Score should be atleast 0"}
   belongs_to :selected_league
   belongs_to :match
+
+  def check_for_matchdate
+    current_match_date = Match.find(match_id).match_date
+  	if current_match_date < Date.today
+  		errors.add(:base, "cannot predict ")
+    end	
+  end
 end
