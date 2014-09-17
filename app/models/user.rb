a4d6aefa21b81add34bfb63137789efed4f3a7ce
Basil Bose <basil@basil-Vostro-1015.(none)>
2014-09-17 12:50:11 +0530

    Omniauth email authentication updated for facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,7 +19,7 @@ class User < ActiveRecord::Base
       user.password = Devise.friendly_token[0, 20]
       user.firstname = auth.info.name   # assuming the user model has a name
       # user.image = auth.info.image # assuming the user model has an image
-      if User.where(:email => user.email).blank?
+      if User.where(:email => user.email, :provider => nil).blank?
         user.save!
       else
         return false
